8fc403816be2928efaedf7307675781bfc8d255e
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-10-19 19:58:06 +0600

    s

--- src/PrivateRoute/PrivateRoute.js
+++ src/PrivateRoute/PrivateRoute.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Redirect, Route } from 'react-router';
 import useAuth from '../context/useAuth';
 
 const PrivateRoute = ({ children, ...rest }) => {
-    let { user } = useAuth();
+    let { user, isLoading } = useAuth();
+    if (isLoading) {
+        return <>
+            {Array.from({ length: 5 }).map((_, idx) => (
+                <Spinner animation="border" variant="info" className='mx-2' />
+            ))}
+        </>
+    }
     return (
         <Route
             {...rest}

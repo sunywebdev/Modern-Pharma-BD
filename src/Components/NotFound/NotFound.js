3c8d6fcc81e8b822c166e30a43281a652ca2c8fb
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2021-10-20 16:13:10 +0600

    404

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -6,12 +6,15 @@ const NotFound = () => {
     return (
         <Container>
             <div className="mx-auto py-5 my-5">
-                <h2 className="fs-1 fw-bold my-5">
+                <h2 className="fs-1 fw-bold mt-5 text-1">
                     404 Not Found
                 </h2>
+                <p className="mb-5 text-1 fw-bold">
+                    Make sure you entered a correct url
+                </p>
             </div>
             <Link to='/home' className='text-decoration-none text-light'>
-                <Button className='my-4 px-5 bg-info border-info'><i class="fas fa-arrow-circle-left me-3"></i>Back To Home</Button>
+                <Button className='my-4 px-5 bg-1 border-1'><i class="fas fa-arrow-circle-left me-3"></i>Back To Home</Button>
             </Link>
         </Container>
     );
